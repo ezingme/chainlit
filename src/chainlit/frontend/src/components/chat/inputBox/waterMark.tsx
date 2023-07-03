f1579b9128b1864b282d5049366708a5e7f63696
ezingme <58387284+ezingme@users.noreply.github.com>
2023-07-03 20:50:27 +0800

    Update waterMark.tsx

--- src/chainlit/frontend/src/components/chat/inputBox/waterMark.tsx
+++ src/chainlit/frontend/src/components/chat/inputBox/waterMark.tsx
@@ -14,12 +14,8 @@ export default function WaterMark() {
         }}
       >
         <Typography fontSize="12px" color="text.secondary">
-          Built with
+          Copyright © 2023 智谱沧渊
         </Typography>
-        <Logo
-          width={65}
-          style={{ filter: 'grayscale(1)', marginLeft: '4px' }}
-        />
       </a>
     </Stack>
   );
